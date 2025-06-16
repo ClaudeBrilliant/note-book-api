e7fe47cc5ad6e66c77bbf70f3cfa62b7013878f2
ClaudeBrilliant <claudennyongesa7@gmail.com>
2025-06-16 22:55:47 +0300

    Swagger api implementation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Your API Title')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('your-tag') // Optional tag for grouping
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // '/api' is the path for Swagger UI
+
+  await app.listen(3000);
 }
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
